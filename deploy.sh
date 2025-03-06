e3f011ead7256013bbcbc6367c007e7a5dbd4ae9
Dhawal Chheda <dhawalc@users.noreply.github.com>
2025-03-06 13:10:01 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -25,9 +25,7 @@ gcloud run deploy ${SERVICE_NAME} \
   --region ${REGION} \
   --allow-unauthenticated \
   --memory 1Gi \
-  --set-env-vars "SNOWFLAKE_ACCOUNT=OU87598,SNOWFLAKE_USERNAME=APP_SCDATA,SNOWFLAKE_PASSWORD=LaPxg9pgf2czyYw7btyNaJadGD7TrGw,SNOWFLAKE_WAREHOUSE=COMPUTE_WH,SNOWFLAKE_DATABASE=chainsyncdb,SNOWFLAKE_SCHEMA=scm,SNOWFLAKE_ROLE=APP_SCDATA_ROLE,SNOWFLAKE_REGION=west-us-2.azure,FRONTEND_URL=http://localhost:3000" \
-  --set-secrets RSA_PRIVATE_KEY=projects/340970450219/secrets/rsa_private_key:latest
-
+  --set-env-vars ""
 echo "Deployment complete. Retrieving Cloud Run URL..."
 URL=$(gcloud run services describe ${SERVICE_NAME} --platform managed --region ${REGION} --format 'value(status.url)')
 echo "Cloud Run service URL: $URL"
